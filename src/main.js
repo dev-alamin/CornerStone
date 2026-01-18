1c2f1ae86628fbd929186c703cfc2bc977db6289
Al Amin <hmalaminmb4@gmail.com>
2026-01-18 21:37:45 +0600

    AlpineJS added, features page, why choose tab section done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import "../js/header.js";
+import Alpine from 'alpinejs'
 import { initAccordion } from "./scripts/accordion.js";
 import { initLogoSlider } from "./scripts/infiniteslider.js";
 import { initTabs } from "./scripts/tab.js";
+
+ 
+window.Alpine = Alpine
+Alpine.start()
 
 document.addEventListener("DOMContentLoaded", () => {
   initLogoSlider();
