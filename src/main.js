857a54543537d72cf3ea12397c01ad636ea789ec
Al Amin <hmalaminmb4@gmail.com>
2026-01-23 22:23:21 +0600

    Pricing page: Finalization

--- src/main.js
+++ src/main.js
@@ -2,19 +2,20 @@ import "../js/header.js";
 import Alpine from 'alpinejs'
 // import Swiper from 'swiper';
 // import 'swiper/css';
+// import gsap from "gsap";
 import { initAccordion } from "./scripts/accordion.js";
 import { initLogoSlider } from "./scripts/infiniteslider.js";
 import { initTabs, initLogoTabs } from "./scripts/tab.js";
 
- 
+
 window.Alpine = Alpine
 Alpine.start()
 
 document.addEventListener("DOMContentLoaded", () => {
-  initLogoSlider();
-  initTabs();
-  initLogoTabs();
-  initAccordion();
+    initLogoSlider();
+    initTabs();
+    initLogoTabs();
+    initAccordion();
 });
 
 new Swiper('.tools-swiper', {
@@ -26,7 +27,7 @@ new Swiper('.tools-swiper', {
         delay: 0,
         disableOnInteraction: false,
     },
-     slidesPerView: 6,
+    slidesPerView: 6,
 });
 
 new Swiper('.tools-swiper-reverse', {
@@ -38,5 +39,28 @@ new Swiper('.tools-swiper-reverse', {
         delay: 0,
         disableOnInteraction: false,
     },
-     slidesPerView: 7,
+    slidesPerView: 7,
+});
+
+// Pricing pageXOffset, Compare section 
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.matchMedia({
+    "(min-width: 1024px)": function () {
+        let panels = document.querySelectorAll('.stack-panel');
+
+        panels.forEach((panel, i) => {
+            gsap.to(panel, {
+                scrollTrigger: {
+                    trigger: panel,
+                    pin: true,           // Pins the card
+                    pinSpacing: false,   // Allows next card to overlap
+                    start: "top 20%",    // Adjust where it sticks
+                    endTrigger: "#comparison-area",
+                    end: "bottom 80%",
+                    scrub: 1
+                }
+            });
+        });
+    }
 });
